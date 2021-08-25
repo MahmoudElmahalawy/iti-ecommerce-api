c6e8be454647a6c84089131adc0ce46aab5f821d
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-08-25 16:04:01 +0200

    Added image, title and description fields to category model

--- models/category.js
+++ models/category.js
@@ -4,6 +4,22 @@ const categorySchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
+		trim: true,
+	},
+	image: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	title: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	description: {
+		type: String,
+		required: true,
+		trim: true,
 	},
 });
 
